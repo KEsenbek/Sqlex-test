f6b673bdb18214d4227f2a4e2a4b0547e56fe79e
Esenbek Kerimov <esenbekerimov@gmail.com>
2022-08-14 22:18:32 +0600

    Первый раз работа с БД

---
+++ src/main/java/kg/megacom/sqlextest/models/Pc.java
@@ -0,0 +1,29 @@
+package kg.megacom.sqlextest.models;
+
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+@Entity
+@Table(name = "pc")
+public class Pc {
+
+    @Id
+Integer code;
+int speed;
+int ram;
+double hd;
+@Column(length = 10)
+String cd;
+double price;
+
+@ManyToOne
+        @JoinColumn(name = "model")
+Product product;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into Printer(code,model,color,type,price) values(1,'1276','n','Laser',400);
+insert into Printer(code,model,color,type,price) values(2,'1433','y','Jet',270);
+insert into Printer(code,model,color,type,price) values(3,'1434','y','Jet',290);
+insert into Printer(code,model,color,type,price) values(4,'1401','n','Matrix',150);
+insert into Printer(code,model,color,type,price)values(5,'1408','n','Matrix',270);
+insert into Printer(code,model,color,type,price) values(6,'1288','n','Laser',400);
